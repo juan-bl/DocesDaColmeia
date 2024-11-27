4feaea4932ba5a1b30aaea4b8a2e787c087d16ea
helen2411 <helen.l.gomes@aluno.senai.br>
2024-11-27 17:22:24 -0300

    Atualização Login

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,8 @@
 // Selecionando os elementos pelo ID
 const inputNome = document.getElementById('usuario');
 const inputEmail = document.getElementById('senha');
+const button=document.getElementById('logar')
+
 
 // Acessando o valor dos inputs
 console.log('Nome de usuário:', inputNome.value);
@@ -16,4 +18,12 @@ console.log('Senha:', inputEmail.value);
 // Adicionando um evento de mudança
 inputNome.addEventListener('change', () => {
   console.log('Novo valor do nome:', inputNome.value);
+});
+
+inputEmail.addEventListener('change', () => {
+  console.log('Novo valor do email:', inputNome.value);
+});
+
+button.addEventListener('click', () => {
+  alert('Login Efetuado!')
 });
