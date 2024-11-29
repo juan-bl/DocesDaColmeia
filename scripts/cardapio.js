72bb2178b686ebf795a5b704e6525b94724f50a9
Juan Nícolas <129800235+juan-bl@users.noreply.github.com>
2024-11-29 17:53:26 -0300

    Carrinho logica

--- scripts/cardapio.js
+++ scripts/cardapio.js
@@ -1,3 +1,5 @@
+const carrinho = []
+
 const lista = [
     ['../images/bolo pelado.png', 'Doce Fino', 50],
     ['../images/Bolo-no-pote.jpeg', 'Bolo no Pote', 10],
@@ -12,7 +14,12 @@ lista.forEach(card => {
         <h3>${card[1]}</h3>
         <p>R$ ${card[2]},00</p>
         <br>
-        <button>ADICIONAR</button>
+        <button type="button" onclick="adicionarProduto('${card[1]}', '${card[2]}')">ADICIONAR</button>
     </div>
     `
 });
+
+function adicionarProduto(nomeBolo, valor){
+    alert('Item adicionado ao carrinho!')
+    carrinho.push([nomeBolo, Number(valor)]) 
+}
